ac191b1f28dc9983cdf9a5c63c2c9547f3cdf447
connorfong <connorfong93@gmail.com>
2015-05-25 22:02:25 -0700

    Create example data, put into ArrayList

--- app/src/main/java/com/example/tacosrkewl/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/tacosrkewl/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +22,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 78/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHER STATION - 68/51",
+                "Sun - Sunny - 80/68"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
